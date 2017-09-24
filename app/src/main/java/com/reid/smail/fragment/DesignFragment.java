01007fa5b8d68e85cd744453701caaea0179beb7
wgx3378 <wgx3378@163.com>
2017-09-24 11:14:10 +0800

    BugFix: 修复DesignFragment Tab 显示问题

--- app/src/main/java/com/reid/smail/fragment/DesignFragment.java
+++ app/src/main/java/com/reid/smail/fragment/DesignFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,6 +40,7 @@ public class DesignFragment extends BaseFragment {
 
     private void setupTabLayout(View view) {
         mTabLayout = (TabLayout) view.findViewById(R.id.home_tab);
+        mTabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
         if (mViewPager == null) {
             setupViewPager(view);
         }
